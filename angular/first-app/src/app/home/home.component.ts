80075017d15a276222c3136dcf92adfd5b01800c
Gokul Krishna <gokris165@gmail.com>
2024-04-01 17:20:51 -0700

    finished step 9 of angular tutorial

--- angular/first-app/src/app/home/home.component.ts
+++ angular/first-app/src/app/home/home.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housing-location';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    HousingLocationComponent
+  ],
   template: `
     <section>
       <form>
@@ -11,9 +18,21 @@ import { Component } from '@angular/core';
         <button class="primary" type="button">Search</button>
       </form>
     </section>
+    <section class="results">
+      <app-housing-location 
+        *ngFor="let housingLocation of housingLocationList"
+        [housingLocation]="housingLocation">
+        1
+      </app-housing-location>
+    </section>
   `,
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+  
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
 }
