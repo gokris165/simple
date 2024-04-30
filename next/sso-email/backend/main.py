5ea9cf203fae5c0e1d1df05a7a8fd09bae3404b6
Gokul Krishna <gokris165@gmail.com>
2024-04-29 18:48:19 -0700

    added basic plaintext email sending functionality in isolation, need to refactor code to run with uvicorn

--- next/sso-email/backend/main.py
+++ next/sso-email/backend/main.py
@@ -1,11 +1,30 @@
 from fastapi import FastAPI, Request
 from dotenv import load_dotenv
+import smtplib
+import ssl
 import os
 
+
+# read environment variables
 load_dotenv()
 
-my_value = os.environ["MY_VALUE"]
-print(my_value)
+email = os.environ["SERVER_GMAIL_ADDRESS"]
+app_passwd = os.environ["SERVER_GMAIL_APP_PASSWD"]
+
+context = ssl.create_default_context()
+
+message = """\
+Subject: Hello from python
+
+This message is sent from Python!
+
+Best,
+Broccoli Test
+"""
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(email, app_passwd)
+    server.sendmail(email, "broccolitest524+client@gmail.com", message)
 
 app = FastAPI(title="sso-email backend")
 
